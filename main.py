503604432855035807904678c2d3329faa385ad6
Jeevan Joseph <81223681+Jeevan1310@users.noreply.github.com>
2022-02-05 15:10:29 +0000

    second commit

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == bot.user:
         return
-    if message.content.startswith('hello'):
-      await message.channel.send('Hello !!!')
+    
     if message.content.startswith(f'kop'):
       await message.channel.send(f'Enna pattiyeda enna prashnam {message.author.mention}')
     
